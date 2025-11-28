7f9c000aa343fef60f3ead0989db197a3c43328c
jeong-sang-won-2003 <jjsw2003@naver.com>
2025-11-28 16:14:01 +0900

    Create ex02.java

---
+++ 14-java/src/day01/ex02.java
@@ -0,0 +1,15 @@
+package day01;
+
+public class ex02 {
+    public static void main(String[] args){
+        int x = 10,y =3;
+
+        System.out.printf("x = "+x+",y = "+y);
+        System.out.println("x + y ="+(x+y));
+        System.out.println("x - y ="+(x-y));
+        System.out.println("x * y ="+(x*y));
+        System.out.println("x / y ="+(x/y));
+        System.out.println("x % y ="+(x%y));
+
+    }
+}
